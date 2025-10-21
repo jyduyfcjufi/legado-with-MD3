2423cda654177c05dcb61224491edf763ed7838f
HapeLee <1321903405@qq.com>
2025-10-22 01:22:15 +0800

    优化ShortCuts显示 #108

--- app/src/main/java/io/legato/kazusa/api/ShortCuts.kt
+++ app/src/main/java/io/legato/kazusa/api/ShortCuts.kt
@@ -23,7 +23,7 @@ object ShortCuts {
         return ShortcutInfoCompat.Builder(context, "bookshelf")
             .setShortLabel(context.getString(R.string.bookshelf))
             .setLongLabel(context.getString(R.string.bookshelf))
-            .setIcon(IconCompat.createWithResource(context, R.drawable.ic_shortcut_books))
+            .setIcon(IconCompat.createWithResource(context, R.mipmap.ic_st_book))
             .setIntent(bookShelfIntent)
             .build()
     }
@@ -34,7 +34,7 @@ object ShortCuts {
         return ShortcutInfoCompat.Builder(context, "lastRead")
             .setShortLabel(context.getString(R.string.last_read))
             .setLongLabel(context.getString(R.string.last_read))
-            .setIcon(IconCompat.createWithResource(context, R.drawable.ic_shortcut_recent))
+            .setIcon(IconCompat.createWithResource(context, R.mipmap.ic_st_rec))
             .setIntents(arrayOf(bookShelfIntent, readBookIntent))
             .build()
     }
@@ -45,7 +45,7 @@ object ShortCuts {
         return ShortcutInfoCompat.Builder(context, "readAloud")
             .setShortLabel(context.getString(R.string.read_aloud))
             .setLongLabel(context.getString(R.string.read_aloud))
-            .setIcon(IconCompat.createWithResource(context, R.drawable.ic_shortcut_read_aloud))
+            .setIcon(IconCompat.createWithResource(context, R.mipmap.ic_st_aloud))
             .setIntent(readAloudIntent)
             .build()
     }
